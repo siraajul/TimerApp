d13dc7784bd4bbe80504ae0293d03429a0c289c2
siraajul <shirajulislamparvez@gmail.com>
2024-06-22 14:20:02 +0600

    Initial Commit

---
+++ TimerApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TimerApp
+//
+//  Created by Sirajul Islam on 22/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        //--Top to Bottom
+        ZStack{
+            //Background Color
+            Color(
+                red:63/255, green: 68/255, blue: 3/255
+            )
+            .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Sirajul Islam on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
