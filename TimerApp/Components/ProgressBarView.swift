318b91fb197bb11a6b40abab5a56a42704a3df8f
siraajul <shirajulislamparvez@gmail.com>
2024-06-22 16:15:00 +0600

    Build---> ProgressBarView

---
+++ TimerApp/Components/ProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarView.swift
+//  TimerApp
+//
+//  Created by Sirajul Islam on 22/6/24.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBarView()
+}
